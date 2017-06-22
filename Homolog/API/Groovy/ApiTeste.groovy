4aa469ad626b8bdc90e1097d63077a0caaff231a
Diego Assumpcao Rodrigues <diego.rodrigues@medbarra.com.br>
2017-06-22 15:23:50 -0300

    atualização

---
+++ Homolog/API/Groovy/ApiTeste.groovy
@@ -0,0 +1,63 @@
+@Grapes([
+    @Grab(group='commons-io', module='commons-io', version='2.5'),
+    @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7'),
+    @Grab('net.sf.json-lib:json-lib:2.3:jdk15')
+    
+])
+
+import groovy.json.JsonSlurper;
+import org.apache.commons.io.IOUtils;
+import groovyx.net.http.*;
+import groovyx.net.http.ContentType.*;
+import groovyx.net.http.Method.*;
+import net.sf.json.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.BufferedInputStream.*;
+
+node {
+    stage("VerificaSeTemBuild"){
+      echo VerificaDir("C:\\PublishApi");
+    }
+	stage("Backup") {
+        echo "Backup"
+		bat 'powershell "C:\\Users\\PublisherTS\\Desktop\\Shells\\teste.ps1" -Verbose'
+		//bat 'powershell "C:\\Works\\PowerShell_testeJenkins\\Teste1.ps1" -Verbose'
+		//PowerShell(". 'C:\\Works\\PowerShell_testeJenkins\\Teste1.ps1'")
+    }
+    stage("build") {
+        echo "build"
+    }
+    stage("teste"){
+        echo "teste"
+    }
+    //bat "powershell -noprofile -command \"$ErrorActionPreference = 'Stop'; Set-PsDebug -Strict; C:\\Works\\PowerShell_testeJenkins\\Teste1.ps1\""
+    //bat "powershell C:\\Works\\PowerShell_testeJenkins\\Teste1.ps1"
+}
+
+@NonCPS
+def VerificaDir(folder) { 
+    
+    def count = 0;
+    
+    new File(folder).eachFile() { file->
+     //println file.getAbsolutePath();
+     count = count + 1;
+    } 
+    
+    if (count>0){
+        //echo "ok";
+        return "ok";
+    }
+    else {
+        //echo "1";
+        return "error";
+    }
+}
